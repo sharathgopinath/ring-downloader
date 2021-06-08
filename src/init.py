7f90f245381b84e4d21f81f2f63ba35e640608ae
sharath <sharathgopinathrao@gmail.com>
2021-06-08 20:21:33 +1000

    Get ring credentials from secrets manager

--- src/init.py
+++ src/init.py
@@ -1,7 +1,34 @@
 import json
+import boto3
+from botocore.exceptions import ClientError
+import sys
 
 def lambda_handler(event, context):
+    secret_data = get_secret()
     return {
         'statusCode': 200,
         'body': json.dumps(f'Hello {event["name"]}!')
     }
+
+def get_secret():
+    secret_name = "RingCredentials"
+    region_name = "ap-southeast-2"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name = 'secretsmanager',
+        region_name = region_name,
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
+    else:
+        text_secret_data = get_secret_value_response['SecretString']
+        return text_secret_data
+    
+    
